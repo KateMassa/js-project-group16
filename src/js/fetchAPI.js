32a3f296543ec7f600f751c3468c0f42f4b33b85
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-02-23 17:06:13 +0100

    зміни + бібліотека в fetchApi.js
    додано метод getCategoryList, виконано рефакторинг з використанням бібліотеки axios

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,56 +1,58 @@
+import axios from 'axios';
+
 export class BooksApi {
   constructor() {
-    this.BASE_URL = 'https://books-backend.p.goit.global';
-    this.BOOKS_ENDPOINT = '/books';
+    this.BASE_URL = 'https://books-backend.p.goit.global/books';
     this.TOP_BOOKS_ENDPOINT = '/top-books';
     this.CATEGORY_ENDPOINT = '/category?category=';
+    this.CATEGORY_LIST_ENDPOINT = '/category-list';
   }
 
-  async getPopularBooks() {
+  async getTopBooks() {
     try {
-      const url = `${this.BASE_URL}${this.BOOKS_ENDPOINT}${this.TOP_BOOKS_ENDPOINT}`;
-      const response = await fetch(url);
+      const url = `${this.BASE_URL}${this.TOP_BOOKS_ENDPOINT}`;
+      const response = await axios.get(url);
 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch popular books: ${response.status}`);
-      }
-
-      return response.json();
+      return response.data;
     } catch (error) {
       console.error('Error fetching popular books:', error);
       throw error;
-    }}
-    
-    async getBooksByCategory(selectedCategory) {
+    }
+  }
+
+  async getBooksByCategory(selectedCategory) {
     try {
-      const url = `${this.BASE_URL}${this.BOOKS_ENDPOINT}${this.CATEGORY_ENDPOINT}${selectedCategory}`;
-      const response = await fetch(url);
+      const url = `${this.BASE_URL}${this.CATEGORY_ENDPOINT}${selectedCategory}`;
+      const response = await axios.get(url);
 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch books for category '${selectedCategory}': ${response.status}`);
-      }
-
-      return response.json();
+      return response.data;
     } catch (error) {
       console.error(`Error fetching books for category '${selectedCategory}':`, error);
       throw error;
     }
   }
 
-    async getDetails(bookId) {
+  async getDetails(bookId) {
     try {
-      const url = `${this.BASE_URL}${this.BOOKS_ENDPOINT}/${bookId}`;
-      const response = await fetch(url);
+      const url = `${this.BASE_URL}/${bookId}`;
+      const response = await axios.get(url);
 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch details for book with ID '${bookId}': ${response.status}`);
-      }
-
-      return response.json();
+      return response.data;
     } catch (error) {
       console.error(`Error fetching details for book with ID '${bookId}':`, error);
       throw error;
     }
   }
-    
+
+  async getCategoryList() {
+    try {
+      const url = `${this.BASE_URL}${this.CATEGORY_LIST_ENDPOINT}`;
+      const response = await axios.get(url);
+
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching category list:', error);
+      throw error;
+    }
+  }
 }
