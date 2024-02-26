ad605391125450a25742fe68bd55a4016849b8b2
KateMassa <catherineorlova27@gmail.com>
2024-02-26 16:33:34 +0100

    upd

--- src/js/header.js
+++ src/js/header.js
@@ -7,3 +7,31 @@ const onOpen = () => {
 const onClose = () => {
   menu.classList.remove('is-open');
 };
+
+// Theme switcher in header
+
+const themeSwitch = document.getElementById('themeSwitch');
+
+// When loading the page, we check the saved topic
+window.addEventListener('DOMContentLoaded', function () {
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    if (currentTheme === 'dark-theme') {
+      themeSwitch.checked = true;
+    }
+  }
+});
+
+// When the switch state changes
+themeSwitch.addEventListener('change', function () {
+  if (this.checked) {
+    document.body.classList.add('dark-theme');
+    document.body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light-theme');
+  }
+});
