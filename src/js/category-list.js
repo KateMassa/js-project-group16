babf1b8e5f9005379d069e5c21a75881e3f1aca5
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-02-27 00:02:32 +0100

    Categories list update + fix

--- src/js/category-list.js
+++ src/js/category-list.js
@@ -1,35 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
-  fetchCategories();
+import { BooksApi } from "./fetchAPI";
+import { renderBestsellers } from "./bestsellers";
+import { renderCategory } from "./renderCategory";
+
+const booksApi = new BooksApi();
+const listContainer = document.getElementById('category-list');
+const allCategories = document.getElementById('all-categories')
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const categories = await booksApi.getCategoryList();
+  displayCategories(categories);
+
+  const allCategories = document.getElementById('all-categories')
+  allCategories.classList.add('active');
+  
+  // === Обробка кліку по категорії "All Categories"
+  allCategories.addEventListener('click', function () {
+    renderBestsellers();
+  });
 });
 
-const apiUrl = 'https://books-backend.p.goit.global/books/category-list';
-
-async function fetchCategories() {
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('HTTP error: ' + response.status);
-    }
-    const categories = await response.json();
-    displayCategories(categories);
-  } catch (error) {
-    alert('Error: ' + error.message);
-  }
-}
-
+// === Створення розмітки для списку категорій книг
 function displayCategories(categories) {
-  const listContainer = document.getElementById('category-list');
-  const list = document.createElement('ul');
+  const allCategoriesItem = '<li class="category-item" id="all-categories" >All categories</li>'
+  const categoryMarkup = categories.map(category => {
+    return `<li class="category-item">${category.list_name}</li>`;
+  }).join('');
 
-  categories.forEach(category => {
-    const listItem = document.createElement('li');
-    listItem.textContent = category;
-    list.appendChild(listItem);
-  });
+  listContainer.innerHTML = allCategoriesItem + categoryMarkup;
 
-  listContainer.appendChild(list);
+  // ==== Створення слухача подій і обробка кліку по категорії із списку
+  listContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('category-item')) {
+      // ====Видалємо клас "active" з усіх елементів списку
+      const categoryItems = document.querySelectorAll('.category-item');
+      categoryItems.forEach(item => {
+        item.classList.remove('active');
+      });
+
+      // === Додаємо клас "active" до елементу списку, на якому був клік
+      event.target.classList.add('active');
+
+      const categoryName = event.target.textContent;
+      renderCategory(categoryName);
+    }
+});
+
+  
 }
-
-fetchCategories();
-
-console.log(displayCategories(categories));
