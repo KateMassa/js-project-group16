0ad4fffb18ccea91695ec3f389d8686ead4890e0
Mykhailo Onipchenko <95632067+M1SHANIA@users.noreply.github.com>
2024-02-26 15:13:40 +0100

    added scrolling logic and button

---
+++ src/js/scroll-btn.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+    // Показываем или скрываем кнопку в зависимости от положения скролла
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+    // Прокручиваем страницу вверх при клике на кнопку
+    scrollToTopButton.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Плавная прокрутка
+        });
+    });
+});
+//<button id="scroll-to-top"></button>
