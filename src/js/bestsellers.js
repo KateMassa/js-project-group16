94dc43b73f817d6d438aaf18579093f744e6d34c
KarinaSnitsarenko <143505937+KarinaSnitsarenko@users.noreply.github.com>
2024-02-26 15:59:24 +0200

    update is-open

--- src/js/bestsellers.js
+++ src/js/bestsellers.js
@@ -38,7 +38,7 @@ function dataBestsellers(data, limit) {
         let book = `<li class='gallery-book-item' data-bookid="${elem.books[i]._id}">
         <a class="gallery-book-link">
         <div class="preview">
-          <img class="gallery-book-img" data-id="${elem.books[i]._id}" src="${elem.books[i].book_image}" alt="${elem.books[i].title}">
+          <img class="gallery-book-img js-open-modal" data-id="${elem.books[i]._id}" src="${elem.books[i].book_image}" alt="${elem.books[i].title}">
           <div class="actions-card">
         <p class="action-text">quick view</p>
           </div>
