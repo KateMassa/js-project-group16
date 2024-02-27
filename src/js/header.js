80a2f200279f8a8d6e6ab01300dc9d444690925a
KateMassa <catherineorlova27@gmail.com>
2024-02-27 07:23:39 +0100

    upd

--- src/js/header.js
+++ src/js/header.js
@@ -5,9 +5,9 @@ const themeSwitch = document.getElementById('themeSwitch');
 themeSwitch.addEventListener('change', function () {
   const logoIcon = document.querySelector('.logo-icon use');
   if (this.checked) {
-    logoIcon.setAttribute('href', './img/icons.svg#icon-logo-dark');
+    logoIcon.setAttribute('href', '../img/icons.svg#icon-logo-dark');
   } else {
-    logoIcon.setAttribute('href', './img/icons.svg#icon-logo-light');
+    logoIcon.setAttribute('href', '../img/icons.svg#icon-logo-light');
   }
 });
 
@@ -34,3 +34,58 @@ themeSwitch.addEventListener('change', function () {
     localStorage.setItem('theme', 'light-theme');
   }
 });
+
+const homeLink = document.querySelector('.nav-menu');
+export let currentUrl = window.location.href;
+const links = homeLink.getElementsByTagName('a');
+function currentPage() {
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href === currentUrl) {
+      links[i].classList.add('current');
+    }
+  }
+}
+
+currentPage();
+
+export const buttonClose = document.querySelector('.btn-close');
+export const buttonMenu = document.querySelector('.burger-menu');
+const modalMenu = document.querySelector('.menu-section');
+import { currentUrl } from './header';
+const shoppingListPage = document.querySelector('.nav-shopping');
+const homeListPage = document.querySelector('.nav-home');
+console.log(shoppingListPage);
+buttonMenu.addEventListener('click', openModalMenu);
+buttonClose.addEventListener('click', closeModalMenu);
+
+function closeModalMenu() {
+  modalMenu.classList.remove('is-open');
+  modalMenu.classList.add('hidden');
+  buttonMenu.classList.remove('hidden');
+  buttonClose.classList.add('hidden');
+}
+
+function openModalMenu() {
+  modalMenu.classList.add('is-open');
+  modalMenu.classList.remove('hidden');
+  buttonMenu.classList.add('hidden');
+  buttonClose.classList.remove('hidden');
+}
+
+// mob-menu current page//
+function mobMenuOnCurrentPage() {
+  if (shoppingListPage.href === currentUrl) {
+    buttonClose.classList.add('hidden');
+    modalMenu.classList.add('hidden');
+    buttonMenu.classList.remove('hidden');
+    buttonMenu.addEventListener('click', openModalMenu);
+  }
+
+  if (homeListPage.href === currentUrl) {
+    buttonClose.classList.add('hidden');
+    modalMenu.classList.add('hidden');
+    buttonMenu.classList.remove('hidden');
+    buttonMenu.addEventListener('click', openModalMenu);
+  }
+}
+mobMenuOnCurrentPage();
