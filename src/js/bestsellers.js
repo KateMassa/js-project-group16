ebc83ba564dca9361831cfea4b1331a5ba028dca
KarinaSnitsarenko <143505937+KarinaSnitsarenko@users.noreply.github.com>
2024-02-27 03:43:18 +0200

    to add screenWidth, css

--- src/js/bestsellers.js
+++ src/js/bestsellers.js
@@ -37,12 +37,12 @@ function dataBestsellers(data, limit) {
       for (let i = 0; i < limit && i < elem.books.length; i++) {
         let book = `<li class='gallery-book-item' data-bookid="${elem.books[i]._id}">
         <a class="gallery-book-link">
-        <div class="preview">
-          <img class="gallery-book-img js-open-modal" data-id="${elem.books[i]._id}" src="${elem.books[i].book_image}" alt="${elem.books[i].title}">
-          <div class="actions-card">
-        <p class="action-text">quick view</p>
+        <div class="preview js-open-modal">
+          <img class="gallery-book-img" data-id="${elem.books[i]._id}" src="${elem.books[i].book_image}" alt="${elem.books[i].title}">
+        <div class="actions-card">
+            <p class="action-text">quick view</p>
           </div>
-        </div>
+          </div>
           <div class="content">
             <h3 class="gallery-book-name">${elem.books[i].title}</h3>
             <h4 class="gallery-book-text">${elem.books[i].author}</h4>
