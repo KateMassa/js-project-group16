3901ca78421a90de39b7a6612c4660dc5d40eee0
KateMassa <catherineorlova27@gmail.com>
2024-02-27 08:57:40 +0100

    //

--- src/js/header.js
+++ src/js/header.js
@@ -1,46 +1,46 @@
-const menu = document.getElementById('burger-menu');
+// const menu = document.getElementById('burger-menu');
 
-const onOpen = () => {
-  menu.classList.add('is-open');
-};
+// const onOpen = () => {
+//   menu.classList.add('is-open');
+// };
 
-const onClose = () => {
-  menu.classList.remove('is-open');
-};
+// const onClose = () => {
+//   menu.classList.remove('is-open');
+// };
 
-// Theme switcher in header
+// // Theme switcher in header
 
-const themeSwitch = document.getElementById('themeSwitch');
+// const themeSwitch = document.getElementById('themeSwitch');
 
-themeSwitch.addEventListener('change', function () {
-  const logoIcon = document.querySelector('.logo-icon use');
-  if (this.checked) {
-    logoIcon.setAttribute('href', './img/icons.svg');
-  } else {
-    logoIcon.setAttribute('href', './img/icons.svgt');
-  }
-});
+// themeSwitch.addEventListener('change', function () {
+//   const logoIcon = document.querySelector('.logo-icon use');
+//   if (this.checked) {
+//     logoIcon.setAttribute('href', './img/icons.svg');
+//   } else {
+//     logoIcon.setAttribute('href', './img/icons.svgt');
+//   }
+// });
 
-// When loading the page, we check the saved topic
-window.addEventListener('DOMContentLoaded', function () {
-  const currentTheme = localStorage.getItem('theme');
-  if (currentTheme) {
-    document.body.classList.add(currentTheme);
-    if (currentTheme === 'dark-theme') {
-      themeSwitch.checked = true;
-    }
-  }
-});
+// // When loading the page, we check the saved topic
+// window.addEventListener('DOMContentLoaded', function () {
+//   const currentTheme = localStorage.getItem('theme');
+//   if (currentTheme) {
+//     document.body.classList.add(currentTheme);
+//     if (currentTheme === 'dark-theme') {
+//       themeSwitch.checked = true;
+//     }
+//   }
+// });
 
-// When the switch state changes
-themeSwitch.addEventListener('change', function () {
-  if (this.checked) {
-    document.body.classList.add('dark-theme');
-    document.body.classList.remove('light-theme');
-    localStorage.setItem('theme', 'dark-theme');
-  } else {
-    document.body.classList.remove('dark-theme');
-    document.body.classList.add('light-theme');
-    localStorage.setItem('theme', 'light-theme');
-  }
-});
+// // When the switch state changes
+// themeSwitch.addEventListener('change', function () {
+//   if (this.checked) {
+//     document.body.classList.add('dark-theme');
+//     document.body.classList.remove('light-theme');
+//     localStorage.setItem('theme', 'dark-theme');
+//   } else {
+//     document.body.classList.remove('dark-theme');
+//     document.body.classList.add('light-theme');
+//     localStorage.setItem('theme', 'light-theme');
+//   }
+// });
--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,41 +1,41 @@
-export const buttonClose = document.querySelector('.btn-close');
-export const buttonMenu = document.querySelector('.btn-menu');
-const modalMenu = document.querySelector('.menu-section');
-import { currentUrl } from "./header";
-const shoppingListPage = document.querySelector('.nav-shopping');
-const homeListPage = document.querySelector('.nav-link');
-// console.log(shoppingListPage);
-buttonMenu.addEventListener('click', openModalMenu);
-buttonClose.addEventListener('click', closeModalMenu);
+// export const buttonClose = document.querySelector('.btn-close');
+// export const buttonMenu = document.querySelector('.btn-menu');
+// const modalMenu = document.querySelector('.menu-section');
+// import { currentUrl } from "./header";
+// const shoppingListPage = document.querySelector('.nav-shopping');
+// const homeListPage = document.querySelector('.nav-link');
+// // console.log(shoppingListPage);
+// buttonMenu.addEventListener('click', openModalMenu);
+// buttonClose.addEventListener('click', closeModalMenu);
 
-function closeModalMenu() {
-   modalMenu.classList.remove('is-open');
-   modalMenu.classList.add('hidden');
-   buttonMenu.classList.remove('hidden');
-   buttonClose.classList.add('hidden');
-}
+// function closeModalMenu() {
+//    modalMenu.classList.remove('is-open');
+//    modalMenu.classList.add('hidden');
+//    buttonMenu.classList.remove('hidden');
+//    buttonClose.classList.add('hidden');
+// }
 
-function openModalMenu() { 
-   modalMenu.classList.add('is-open');
-   modalMenu.classList.remove('hidden');
-   buttonMenu.classList.add('hidden');
-   buttonClose.classList.remove('hidden');
-}
+// function openModalMenu() {
+//    modalMenu.classList.add('is-open');
+//    modalMenu.classList.remove('hidden');
+//    buttonMenu.classList.add('hidden');
+//    buttonClose.classList.remove('hidden');
+// }
 
-// mob-menu current page//
-function mobMenuOnCurrentPage() {
-  if (shoppingListPage.href===currentUrl) {
-     buttonClose.classList.add('hidden');
-     modalMenu.classList.add('hidden');
-     buttonMenu.classList.remove('hidden');
-     buttonMenu.addEventListener('click', openModalMenu);
-   };
+// // mob-menu current page//
+// function mobMenuOnCurrentPage() {
+//   if (shoppingListPage.href===currentUrl) {
+//      buttonClose.classList.add('hidden');
+//      modalMenu.classList.add('hidden');
+//      buttonMenu.classList.remove('hidden');
+//      buttonMenu.addEventListener('click', openModalMenu);
+//    };
 
-    if (homeListPage.href===currentUrl) {
-     buttonClose.classList.add('hidden');
-   modalMenu.classList.add('hidden');
-     buttonMenu.classList.remove('hidden');
-     buttonMenu.addEventListener('click', openModalMenu);
-   } 
-}
-mobMenuOnCurrentPage()
+//     if (homeListPage.href===currentUrl) {
+//      buttonClose.classList.add('hidden');
+//    modalMenu.classList.add('hidden');
+//      buttonMenu.classList.remove('hidden');
+//      buttonMenu.addEventListener('click', openModalMenu);
+//    }
+// }
+// mobMenuOnCurrentPage()
