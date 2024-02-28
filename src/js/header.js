8d42986c0258b97940da6e65183421206a4ffc29
KateMassa <catherineorlova27@gmail.com>
2024-02-28 16:04:43 +0100

    Revert "Merge branch 'test-branch'"
    This reverts commit 6c759ce0fed505009417c5f61177ee3c00b06cf7, reversing
    changes made to 133f963f3c5b2eda910b37f2d80a3668d31de935.

--- src/js/header.js
+++ src/js/header.js
@@ -35,6 +35,65 @@ themeSwitch.addEventListener('change', function () {
   }
 });
 
+// const homeLink = document.querySelector('.nav-menu');
+// export let currentUrl = window.location.href;
+// const links = homeLink.getElementsByTagName('a');
+// function currentPage() {
+//   for (let i = 0; i < links.length; i++) {
+//     if (links[i].href === currentUrl) {
+//       links[i].classList.add('current');
+//     }
+//   }
+// }
+
+// currentPage();
+
+// export const buttonClose = document.querySelector('.btn-close');
+// export const buttonMenu = document.querySelector('.burger-menu');
+// const modalMenu = document.querySelector('.menu-section');
+// import { currentUrl } from './header';
+// const shoppingListPage = document.querySelector('.nav-shopping');
+// const homeListPage = document.querySelector('.nav-home');
+// console.log(shoppingListPage);
+// buttonMenu.addEventListener('click', openModalMenu);
+// buttonClose.addEventListener('click', closeModalMenu);
+
+// function closeModalMenu() {
+//   modalMenu.classList.remove('is-open');
+//   modalMenu.classList.add('hidden');
+//   buttonMenu.classList.remove('hidden');
+//   buttonClose.classList.add('hidden');
+// }
+
+// function openModalMenu() {
+//   modalMenu.classList.add('is-open');
+//   modalMenu.classList.remove('hidden');
+//   buttonMenu.classList.add('hidden');
+//   buttonClose.classList.remove('hidden');
+// }
+
+// mob-menu current page//
+// function mobMenuOnCurrentPage() {
+//   if (shoppingListPage.href === currentUrl) {
+//     buttonClose.classList.add('hidden');
+//     modalMenu.classList.add('hidden');
+//     buttonMenu.classList.remove('hidden');
+//     buttonMenu.addEventListener('click', openModalMenu);
+//   }
+
+//   if (homeListPage.href === currentUrl) {
+//     buttonClose.classList.add('hidden');
+//     modalMenu.classList.add('hidden');
+//     buttonMenu.classList.remove('hidden');
+//     buttonMenu.addEventListener('click', openModalMenu);
+//   }
+// }
+// mobMenuOnCurrentPage();
+
+
+// const buttonMenu = document.querySelector('.burger-menu');
+// const modalMenu = document.querySelector('.menu-section');
+// const buttonClose = document.querySelector('.btn-close');
 
 export const buttonClose = document.querySelector('.btn-close');
 export const buttonMenu = document.querySelector('.burger-menu');
